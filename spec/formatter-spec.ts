795d1c53047a7d2b99ea3707accbf8b36f57b644
Alexei Bykov <alexei4203@yandex.ru>
2016-09-26 22:55:41 +0300

    improve formatter tests

--- spec/formatter-spec.ts
+++ spec/formatter-spec.ts
@@ -2,25 +2,26 @@ import * as fs from 'fs';
 
 import {formatImports} from '../src/formatter';
 
-function doTest(filename: string, verbose: boolean): string[] {
-  let content = fs.readFileSync(filename).toString();
-  let output: string[] = [];
-  formatImports(filename, content, verbose, s => output.push(s));
-  return output;
+function defineTest(name: string, inputPath: string, expectPath: string, verbose: boolean) {
+  it(name, () => {
+    spyOn(console, 'log');
+
+    let input = fs.readFileSync(inputPath).toString();
+    let output: string[] = [];
+    formatImports(inputPath, input, verbose, s => output.push(s));
+
+    let expectedOutput = fs.readFileSync(expectPath).toString();
+    expect(output.join('')).toEqual(expectedOutput);
+
+    if (verbose) {
+      expect(console.log).toHaveBeenCalled();
+    } else {
+      expect(console.log).not.toHaveBeenCalled();
+    }
+  });
 }
 
 describe('Formatter suite', () => {
-  it('Single identifiers', () => {
-    spyOn(console, 'log');
-    let output = doTest('spec/support/test.ts', true);
-    expect(output).toEqual(jasmine.any(Array));
-    expect(console.log).toHaveBeenCalled();
-  });
-
-  it('Multiple identifiers', () => {
-    spyOn(console, 'log');
-    let output = doTest('spec/support/test2.ts', false);
-    expect(output).toEqual(jasmine.any(Array));
-    expect(console.log).not.toHaveBeenCalled();
-  });
+  defineTest('Single identifiers', 'spec/support/test.ts', 'spec/support/test.ts.expected', true);
+  defineTest('Single identifiers', 'spec/support/test2.ts', 'spec/support/test2.ts.expected', false);
 });
